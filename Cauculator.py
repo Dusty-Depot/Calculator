0ca5a9da2a61e4eff2f4361d612a5d183c1f3c26
Dusty-Depot <elliott.agrest@outlook.com>
2024-06-08 11:53:45 -0400

    hi

---
+++ Cauculator.py
@@ -0,0 +1,20 @@
+'''
+1st number
+operation
+2nd number
+'''
+
+firstNumber = int(input("What is your first number?"))
+operation = input("what is your operation?")
+SecondNumber = int(input("what is your second number?"))
+
+if operation == "+":
+    print(firstNumber + SecondNumber)
+elif operation == "-":
+    print(firstNumber - SecondNumber)
+elif operation == "*":
+    print(firstNumber * SecondNumber)
+elif operation == "/":
+    print(firstNumber / SecondNumber) 
+
+    
